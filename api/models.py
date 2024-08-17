bcf52c5b8bf1c3eb1842031a16302a681d69b903
venkatesh <venkateshpensalwar561@gmail.com>
2024-08-17 09:57:51 +0530

    Task

--- api/models.py
+++ api/models.py
@@ -13,6 +13,7 @@ class Client(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='clients')
+    updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.name
